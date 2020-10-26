0e569463a0258e231364ccb973754fbaafd76925
purushothaman-source <purushoth99jun@gmail.com>
2020-10-26 07:59:14 +0530

    changes made in review sh file

--- review.sh
+++ review.sh
@@ -1,16 +1,35 @@
 #!/bin/bash 
 read -p "enter the three digit number " n
-j=0
-o=0
-z=1
+index=0
+temp=0
+temp1=0
+palindrome(){
+        num=$1
+     reversedInteger=0
+
+        while [ $num -ne 0 ]
+        do
+            remainder=$(($num % 10))
+            reversedInteger=$(($reversedInteger * 10 + $remainder))
+            num=$(($num/10))
+        done
+if [ $1 -eq  $reversedInteger ]
+       then
+            echo   " $1  is a palindrome "
+        else
+            echo  " $1 is not a palindrome."
+ fi
+}
+
+
 if [[ n -gt 99 && n -le 1000 ]]
 then
 for (( i=1 ; $(( i<$n ));i++))
 do
 if [ $((n%i)) -eq 0 ]
 then
-  factors[$j]=$i
-  ((j++))
+  factors[index]=$i
+  ((index++))
 fi
 done
 fi
@@ -20,15 +39,21 @@ for((j=0;j< ${#factors[@]};j++))
 do
     if [[ $((factors[j]%2)) -eq 0 ]] 
      then
-       even[$o]=${factors[j]}
-       o=$((o+2))
+       even[temp]=${factors[j]}
+       palindrome ${even[temp]}
+       ((temp++))
      
      else
-        odd[$z]=${factors[j]}
+        
+        odd[temp1]=${factors[j]}
+        palindrome ${odd[temp1]}
+        ((temp1++))
      fi
 done
 
 echo ${even[@]}
 echo ${odd[@]}
 
- 
+
+
+
